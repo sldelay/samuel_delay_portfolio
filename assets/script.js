e7bc8b793a5a05e9284dfca13912cd28a5d6321c
Sam Delay <samuel.delay@gmail.com>
2020-02-19 19:55:40 -0500

    first js commit

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+
+    let currentDay = moment().format('dddd');
+    $(".greeting").text(`Hope you are having a great ${currentDay}!`)
+  });
