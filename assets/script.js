6e25291e311383504e3cbf775447ee626e041ab1
Sam Delay <samuel.delay@gmail.com>
2020-04-16 21:14:22 -0400

    no more moment

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,6 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
 
-    let currentDay = moment().format('dddd');
-    $(".greeting").text(`Hope you are having a great ${currentDay}!`)
+    // let currentDay = moment().format('dddd');
+    // $(".greeting").text(`Hope you are having a great ${currentDay}!`)
   });
